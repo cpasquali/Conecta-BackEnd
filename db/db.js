bcbf78b58a36b2529a0ef1337c470ad8df9ae2b7
cpasquali <conipasquali2@gmail.com>
2025-08-21 20:52:45 -0300

    cambios en la db.js

--- db/db.js
+++ db/db.js
@@ -3,6 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = await mysql2.createConnection(process.env.MYSQL_URL);
+const db = await mysql2.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+});
 
 export default db;
