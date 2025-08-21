9f47c6c7e6bb4807cc2d2138c5f52dae27b940c9
cpasquali <conipasquali2@gmail.com>
2025-08-21 20:26:40 -0300

    cambios minimos

--- db/db.js
+++ db/db.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = await mysql2.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 export default db;
